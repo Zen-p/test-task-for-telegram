4109b5850d354af5d1b56d81d59c281b1ab41efc
evgenijpesko <peshkozena66@gmail.com>
2025-05-26 11:25:18 +0300

    добавлена директория entities
    создан класс user с полями id, firstName, lastName, username

--- src/main/java/com/test/telegram/entities/User.java
+++ src/main/java/com/test/telegram/entities/User.java
@@ -1,6 +1,39 @@
 package com.test.telegram.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
+@Entity
+@Data
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @SequenceGenerator (
+        name = "user_sequence_generator",
+        sequenceName = "user_sequence"
+            )
+    @GeneratedValue (
+        strategy = GenerationType.SEQUENCE,
+        generator = "user_sequence_generator"
+            )
+    private Long id;
+
+    @Size(min = 2, max = 50, message = "First name cannot exceed 50 characters")
+    @NotBlank(message = "First name is required")
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+
+    @Size(min = 2, max = 50, message = "Last name cannot exceed 50 characters")
+    @NotBlank(message = "Last name is required")
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    private String username;
 }
